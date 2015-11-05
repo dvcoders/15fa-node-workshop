9bdfda6507dc9b3894b9df325ca52d89057a18bb
Jake Loo <slow-@live.com>
2015-11-04 16:58:23 -0800

    database, post, get

--- routes.js
+++ routes.js
@@ -1,14 +1,62 @@
 module.exports = function(app) {
   
+  // Note : Not a good practice to place logics in the routes.
+  // Commonly each handler has its own js file.
+  
+  var studentDatabase = {};
+  
   app.get('/', function(req, res) {
-    var name = req.query.name || "you";
+    var studentId = req.query.studentId;
+    
+    if (!studentId) {
+      res.status(400).json({
+        code: 400,
+        message: "Bad Request. Invalid studentId"
+      });
+      return;
+    }
 
-    res.json({ 
-      message: 'Hey ' + name + ", how are you?"
-    });
+    if (!(studentId in studentDatabase)) {
+      res.status(404).json({
+        code: 404,
+        message: "Not Found. Student not found in the database."
+      });
+      return;
+    } 
+
+    res.json(studentDatabase[studentId]);
   });
 
   app.post('/', function(req, res) {
-    res.send(req.body);
+    var body = req.body;
+
+    var studentId = body['studentId'];
+    var studentName = body['studentName'];
+
+    if (!studentId) {
+      res.status(400).json({
+        code: 400,
+        message: "Bad Request. Invalid studentId"
+      });
+      return;
+    }
+
+    if (studentId in studentDatabase) {
+      res.status(409).json({
+        code: 409,
+        message: "Conflict. StudentId already exists in the database."
+      });
+      return;
+    }
+
+    var student = {
+      "studentId" : studentId,
+      "studentName" : studentName
+    };
+
+    studentDatabase[studentId] = student;
+    res.json({
+      message: "Created student (" + studentId +")"
+    });
   });
 };
