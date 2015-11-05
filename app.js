6534196c1f57e489574f2639de3122f70ef9eaf4
Jake Loo <slow-@live.com>
2015-11-04 16:31:02 -0800

    half

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express    = require('express');
 var cors       = require('cors');
 var app        = express();
+var bodyParser  = require('body-parser');
 
 // Set Port
 var port = process.env.PORT || 3000;
 
 // Apply Cors middleware
 app.use(cors());
+
+app.use(bodyParser.json());
 
 // Require all routes in /routes
 require('./routes')(app);
--- routes.js
+++ routes.js
@@ -1,7 +1,14 @@
 module.exports = function(app) {
   
   app.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    var name = req.query.name || "you";
+
+    res.json({ 
+      message: 'Hey ' + name + ", how are you?"
+    });
   });
 
+  app.post('/', function(req, res) {
+    res.send(req.body);
+  });
 };
