bf4c7ef57110f63c661af25f1c42f8fd1159ac57
Jacob Young <jacob@jacobyoung.me>
2015-11-11 14:18:33 -0800

    Form to standard.js syntax

--- node-todo-list/routes.js
+++ node-todo-list/routes.js
@@ -12,20 +12,20 @@ function todoAPI (app) {
     res.sendFile(__dirname + '/todo.js')
   })
 
-  // items is a map of objects 
+  // items is a map of objects
   // Each object will have the following attributes (called keys) and values
 
   // {id:Number, title: String, completed: Bool}
   var items = new Map()
   app.get('/all', function (req, res) {
     var itemArray = []
-    
+
     for (var key in items) {
       itemArray.push(items[key])
     }
 
     res.send({
-      'items' : itemArray
+      'items': itemArray
     })
   })
 
@@ -51,30 +51,28 @@ function todoAPI (app) {
 
   app.put('/item', function (req, res) {
     var body = req.body
-    
+
     // make sure id is given and is in database
     if (!body.id) return sendResponse(res, 400, 'No id included')
     if (!(body.id in items)) return sendResponse(res, 404, 'Item not found')
 
-    // ONLY apply changes to what is provided in the body, otherwise nothing changes 
+    // ONLY apply changes to what is provided in the body, otherwise nothing changes
     if (body.title) items[body.id]['title'] = body.title
     if (body.completed) items[body.id]['completed'] = body.completed
 
-    sendResponse (res, 200, JSON.stringify(items[body.id]))
-    })
+    sendResponse(res, 200, JSON.stringify(items[body.id]))
+  })
 
   app.delete('/item', function (req, res) {
     var body = req.body
-    
+
     if (!body.id) return sendResponse(res, 400, 'No id included')
     if (!(body.id in items)) return sendResponse(res, 404, 'Item not found')
 
-        var copyItem = items[body.id]
-
-      delete items[body.id]
-
-      sendResponse(res, 200, JSON.stringify(copyItem))
-    })
+    var copyItem = items[body.id]
+    delete items[body.id]
+    sendResponse(res, 200, JSON.stringify(copyItem))
+  })
 
   function sendResponse (res, statusCode, message) {
     res.status(statusCode).json({
