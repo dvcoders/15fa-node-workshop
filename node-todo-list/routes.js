f331ba7795795ba511ae4a3d1fbf9a2bd1b08966
Jake Loo <slow-@live.com>
2015-11-11 10:26:52 -0800

    Changes proposal, Add delete endpoint

--- node-todo-list/routes.js
+++ node-todo-list/routes.js
@@ -12,50 +12,63 @@ function todoAPI (app) {
     res.sendFile(__dirname + '/todo.js')
   })
 
-  // items is an Array of Objects
+  // items is a map of objects 
   // Each object will have the following attributes (called keys) and values
 
   // {id:Number, title: String, completed: Bool}
-  var items = []
+  var items = {} 
   app.get('/all', function (req, res) {
-    res.send(items)
+    var itemArray = []
+    
+    for (var key in items) {
+      itemArray.push(items[key])
+    }
+
+    res.send(itemArray)
   })
 
   app.post('/item', function (req, res) {
     var body = req.body
 
     if (!body.title) return sendResponse(res, 400, 'No title included')
-    if (body.completed === '') return sendResponse(res, 400, 'No completion boolean included')
+    if (!body.completed) return sendResponse(res, 400, 'No completion boolean included')
 
     var newItem = {
       'id': (items.length + 1),
       'title': body.title,
       'completed': body.completed
     }
-    items.push(newItem)
+    items[newItem.id] = newItem
     console.log('Successfully posted: ' + JSON.stringify(newItem))
     res.send(newItem)
   })
 
-  app.put('/item/:id?', function (req, res) {
-    matchId(req, res)
+  app.put('/item', function (req, res) {
+    var body = req.body
+    
+    // make sure id is given and is in database
+    if (!body.id) return sendResponse(res, 400, 'No id included')
+    if (!(body.id in items)) return sendResponse(res, 404, 'Item not found')
+
+    // ONLY apply changes to what is provided in the body, otherwise nothing changes 
+    if (body.title) items[body.id]['title'] = body.title
+    if (body.completed) items[body.id]['completed'] = body.completed
+
+    sendResponse (res, 200, JSON.stringify(items[body.id]))
   })
 
-  // Helper functions
-  //
-  function matchId (req, res) {
-    var found = false
-    var searchId = req.params.id ? Number(req.params.id) : Number(req.query.id)
-    items.forEach(function (item) {
-      if (item.id === searchId) {
-        found = true
-        item.title = req.body.title
-        item.completed = req.body.completed
-        sendResponse(res, 200, 'Successfully updated element ' + searchId + ' with ' + req.body.title + ' ' + req.body.completed)
-      }
-    })
-    if (!found) sendResponse(res, 409, 'Item with ID ' + searchId + ' could not be found')
-  }
+  app.delete('/item', function (req, res) {
+    var body = req.body
+    
+    if (!body.id) return sendResponse(res, 400, 'No id included')
+    if (!(body.id in items)) return sendResponse(res, 404, 'Item not found')
+    
+    var copyItem = items[body.id]
+
+    delete items[body.id]
+
+    sendResponse(res, 200, JSON.stringify(copyItem))
+  })
 
   function sendResponse (res, statusCode, message) {
     res.status(statusCode).json({
